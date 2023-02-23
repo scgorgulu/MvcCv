4274d18936481b2ef7f51b1b4ce1dcc5cc8d06c1
Serhat Cagri Gorgulu <cagri.gorgulu@gazi.edu.tr>
2023-02-24 00:30:49 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitim()
+        {
+            var egitimler = db.TblEgitim.ToList();
+            return PartialView(egitimler);
+        }  
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }  
+        public PartialViewResult Sertifikalarım()
+        {
+            var sertifikalar = db.TblSertifikalarım.ToList();
+            return PartialView(sertifikalar);
+        }
+        
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+           
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Iletisim(TblIletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortTimeString());
+            db.TblIletisim.Add(t);
+            db.SaveChanges();
+
+            return PartialView();
+        }
+    }
+}
---
+++ MvcCv/Controllers/YeteneklerimController.cs
@@ -0,0 +1,55 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class YeteneklerimController : Controller
+    {
+        // GET: Yeteneklerim
+       
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();
+            return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YetenekEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YetenekEkle(TblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction ("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            TblYeteneklerim t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGetir(int id)
+        {
+            TblYeteneklerim t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult YetenekGetir(TblYeteneklerim p)
+        {
+            TblYeteneklerim t = repo.Find(x => x.ID == p.ID);
+            t.Yetenek = p.Yetenek;
+            t.Oran = p.Oran;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
